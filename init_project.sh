70d4ca777726b1582e72de740db42cc457df6031
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2019-03-24 21:13:21 +0900

    dockerignore

--- init_project.sh
+++ init_project.sh
@@ -6,6 +6,7 @@ which direnv || echo "ERROR: direnv was not found. Please install it."; exit 1
 git submodule add https://github.com/takutakahashi/deploy-tools.git .deploy
 cat .deploy/.envrc >> .envrc
 cat .deploy/.gitignore >> .gitignore
+cat .deploy/.dockerignore >> .dockerignore
 echo "export APPLICATION=`basename $PWD`" >> .envrc
 mkdir -p bin
 mkdir -p .direnv
