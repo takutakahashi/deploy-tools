8cac790f8156a82d4467eb1268225371a000673b
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2019-03-24 21:09:21 +0900

    mod init project

--- init_project.sh
+++ init_project.sh
@@ -1,4 +1,7 @@
-#!/bin/bash -xe
+#!/bin/bash -e
+
+which docker || echo "ERROR: docker was not found. Please install it."; exit 1
+which direnv || echo "ERROR: direnv was not found. Please install it."; exit 1
 
 git submodule add https://github.com/takutakahashi/deploy-tools.git .deploy
 cat .deploy/.envrc >> .envrc
